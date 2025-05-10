bc7c5270fa428af6bd82b77431aae9f1b7121182
Alex0711 <alex.senger@hotmail.com>
2025-05-10 18:15:04 +0100

    add Services structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,6 @@ module.exports = {
       },
       fontFamily: {
         jost: ["Jost", "sans-serif"],
-      },
-      fontSize: {
-        'title': '2.25rem',
-        'slogan': '4rem',
-        'subtitle': '1.5rem',
-        'body': '1rem',
       }
     },
   },
